c257015b553acda10e8b86ec99da25fc93bef333
Mohamed Ghazal <mohamed.saad.ghazal@gmail.com>
2024-06-30 00:11:13 +0300

    .

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,14 +6,14 @@ match priority:
         if time_bound == "yes":
          print("Reminder: " + str(task) + " is a " + str(priority) + " priority task that requires immediate attention today!")
         elif time_bound == "no":
-         print(str(task) + " is a " + str(priority) + " priority task. Consider completing it when you have free time.")
+         print("Note: " + str(task) + " is a " + str(priority) + " priority task. Consider completing it when you have free time.")
     case "medium":
         if time_bound == "yes":
          print("Reminder: " + str(task) + " is a " + str(priority) + " priority task that requires immediate attention today!")
         elif time_bound == "no":
-         print(str(task) + " is a " + str(priority) + " priority task. Consider completing it when you have free time.")
+         print("Note: " + str(task) + " is a " + str(priority) + " priority task. Consider completing it when you have free time.")
     case "low":
         if time_bound == "yes":
          print("Reminder: " + str(task) + " is a " + str(priority) + " priority task that requires immediate attention today!")
         elif time_bound == "no":
-         print(str(task) + " is a " + str(priority) + " priority task. Consider completing it when you have free time.")   
+         print("Note: " + str(task) + " is a " + str(priority) + " priority task. Consider completing it when you have free time.")   
