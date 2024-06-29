b9512c760c2fed0e414e82a19afba614dad8a524
Mohamed Ghazal <mohamed.saad.ghazal@gmail.com>
2024-06-30 02:11:04 +0300

    added changes

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -3,17 +3,13 @@ priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 match priority:
     case "high":
-        if time_bound == "yes":
-         print("Reminder: " + str(task) + " is a " + str(priority) + " priority task that requires immediate attention today!")
-        elif time_bound == "no":
-         print("Note: " + str(task) + " is a " + str(priority) + " priority task. Consider completing it when you have free time.")
-    case "medium":
-        if time_bound == "yes":
-         print("Reminder: " + str(task) + " is a " + str(priority) + " priority task that requires immediate attention today!")
-        elif time_bound == "no":
-         print("Note: " + str(task) + " is a " + str(priority) + " priority task. Consider completing it when you have free time.")
-    case "low":
-        if time_bound == "yes":
-         print("Reminder: " + str(task) + " is a " + str(priority) + " priority task that requires immediate attention today!")
-        elif time_bound == "no":
-         print("Note: " + str(task) + " is a " + str(priority) + " priority task. Consider completing it when you have free time.")   
+        reminder = f"Reminder: {task} is a high priority task"
+    case "medium" | "low":
+        reminder = f"Note: {task} is a {priority} priority task"
+    case _:
+        raise ValueError("invalid priorty")
+if time_bound == "yes":
+    reminder += f" that requires immediate attention today!"
+else:
+    reminder += f" Consider completing it when you have free time."
+print(reminder)
