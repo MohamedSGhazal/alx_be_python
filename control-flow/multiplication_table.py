d6555679ea5521f7715447ad2141f2e0b7473e5f
Mohamed Ghazal <mohamed.saad.ghazal@gmail.com>
2024-06-29 21:38:47 +0300

    multuplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 for Y in range(1, 11):
  X = number
  Z = X * Y
- print(str(X) + str(" * ") + str(Y) + str(" = ") + str(Z))
+ print(f"{X} * {Y} = {Z}")
