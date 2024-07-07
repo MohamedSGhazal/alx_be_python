0fd0b0351adea13554271d513e636bd51311fa2b
Mohamed Ghazal <mohamed.saad.ghazal@gmail.com>
2024-07-07 03:27:38 +0300

    .

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,6 +6,8 @@ def perform_operation(num1, num2, operation):
         elif operation == "multiply":
             return num1 * num2
         elif operation == "divide":
-            return num1 / num2
+            if num2 == 0:
+                 return "number 2 can't be zero"
+            return num2 / num1
         else:
             raise ValueError("Invalid operation")
