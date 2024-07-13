b489ecd93d3f77576708675c4234eb60adf965c1
Mohamed Ghazal <mohamed.saad.ghazal@gmail.com>
2024-07-13 23:28:54 +0300

    .

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,9 +4,9 @@ class ZeroDivisionError(Exception):
 
 def safe_divide(numerator, denominator):
     try:
-        if type(numerator) not in [int, float] or type(denominator) not in [int, float]:
-            raise ValueError()
-        elif denominator == 0:
+        numerator = float(numerator)
+        denominator = float(denominator)
+        if denominator == 0:
             raise ZeroDivisionError()
         result = numerator / denominator
         return f"The result of the division is {result}"
