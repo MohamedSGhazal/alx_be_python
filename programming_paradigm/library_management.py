396d878e136bf96587e6773d0d0b5a70aa385bc8
Mohamed Ghazal <mohamed.saad.ghazal@gmail.com>
2024-07-14 01:32:20 +0300

    .

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,27 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.__is_checked_out = False
+    def get_is_checked_out(self):
+        return self.__is_checked_out
+    def set_is_checked_out(self, is_checked_out):
+        self.__is_checked_out = is_checked_out
+    
+class Library:
+    def __init__(self):
+        self.__books = []
+    def add_book(self, book:Book):
+        self.__books.append(book)
+    def check_out_book(self, title):
+        for book in self.__books:
+            if book.title == title:
+                book.set_is_checked_out(True)
+    def return_book(self, title):
+        for book in self.__books:
+            if book.title == title:
+                book.set_is_checked_out(False)
+    def list_available_books(self):
+        for book in self.__books:
+            if not book.get_is_checked_out():
+                print(f"{book.title} by {book.author}")
